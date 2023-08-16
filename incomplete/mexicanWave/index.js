8ce2e1f2458186d471d234269089b46828406176
Ian Grantham <dev.iangrantham@gmail.com>
2023-08-16 23:15:55 +0100

    handles single string

---
+++ incomplete/mexicanWave/index.js
@@ -0,0 +1,15 @@
+function wave(str){
+  const stringArray = str.split('')
+  const waveArray = []
+  for (let i = 0; i< stringArray.length; i++) {
+    const waving = stringArray[i].toUpperCase()
+    stringArray[i] = waving
+    stringArray.splice(i, 1, waving)
+    waveArray.push(stringArray.join(''))
+    const reset = stringArray[i].toLowerCase()
+    stringArray.splice(i, 1, reset)
+  };
+  return waveArray
+}
+
+console.log(wave('hello'))
