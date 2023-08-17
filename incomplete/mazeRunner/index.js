013e05760e830ca28a8d4a6ac6a2cacaea0f5ca0
Your Name <your_email@address>
2023-08-17 11:15:15 +0100

    findPath processes directions

--- incomplete/mazeRunner/index.js
+++ incomplete/mazeRunner/index.js
@@ -22,13 +22,14 @@ function mazeRunner(maze, directions) {
   console.log("ENTRANCE", entranceCoordinates);
   const exitCoordinates = findDoor(maze, "exit");
   console.log("EXIT", exitCoordinates);
+  findPath(entranceCoordinates, exitCoordinates, directions);
 }
 
 function findDoor(maze, door) {
   let doorValue = 0;
   let coordinates = {
-    x: 0,
-    y: 0,
+    x: 1,
+    y: 1,
   };
   const whichDoor = door;
   switch (whichDoor) {
@@ -37,7 +38,6 @@ function findDoor(maze, door) {
       break;
     case "exit":
       doorValue = key.exit;
-    default:
       break;
   }
   for (i = 0; i < maze.length; i++) {
@@ -45,13 +45,39 @@ function findDoor(maze, door) {
     let foundDoor = false;
     if (mazeLayer.includes(doorValue)) {
       foundDoor = true;
-      coordinates.x = mazeLayer.indexOf(doorValue);
+      coordinates.x += mazeLayer.indexOf(doorValue);
     }
     if (foundDoor) {
-      coordinates.y = i;
+      coordinates.y += i;
     }
   }
   return coordinates;
+}
+
+function findPath(enterAt, exitAt, directions) {
+  const finishPos = exitAt;
+  let currentPos = enterAt;
+  console.log("finding path from", currentPos, "to", finishPos);
+  directions.forEach((dir) => {
+    switch (dir) {
+      case "N":
+        currentPos.y--;
+        break;
+      case "S":
+        currentPos.y++;
+        break;
+      case "E":
+        currentPos.x++;
+        break;
+      case "W":
+        currentPos.x--;
+        break;
+    }
+    console.log('you are at:', currentPos)
+  });
+  if (currentPos.x === finishPos.x && currentPos.y === finishPos.y) {
+    return console.log('Finish')
+  }
 }
 
 console.log(mazeRunner(maze, directions));
