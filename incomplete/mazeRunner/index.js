6601928e9232b51399cbf0d4ff6155b3b14fa9cf
Your Name <your_email@address>
2023-08-17 16:06:07 +0100

    mazeRunner failing one hidden test

--- incomplete/mazeRunner/index.js
+++ incomplete/mazeRunner/index.js
@@ -16,10 +16,12 @@ const key = {
 };
 
 let safePath = true;
+let outsideMaze = false;
 let finished = false;
 
 function mazeRunner(maze, directions) {
   safePath = true;
+  outsideMaze = false;
   finished = false;
   const entranceCoordinates = findDoor(maze, "entrance");
   const exitCoordinates = findDoor(maze, "exit");
@@ -60,130 +62,177 @@ function findPath(enterAt, exitAt, directions) {
   const finishPos = exitAt;
   let currentPos = enterAt;
   directions.forEach((direction) => {
-    if (!safePath) return "Dead";
-    if (finished) return "Finish";
+    if (!safePath || outsideMaze) return "Dead";
+    else if (finished) return "Finish";
     switch (direction) {
       case "N":
         currentPos.y--;
         checkIsSafe(currentPos);
+        checkInsideMaze(currentPos, directions);
         checkIfFinished(currentPos, finishPos);
         break;
       case "S":
         currentPos.y++;
         checkIsSafe(currentPos);
+        checkInsideMaze(currentPos, directions);
         checkIfFinished(currentPos, finishPos);
         break;
       case "E":
         currentPos.x++;
         checkIsSafe(currentPos);
+        checkInsideMaze(currentPos, directions);
         checkIfFinished(currentPos, finishPos);
         break;
       case "W":
         currentPos.x--;
         checkIsSafe(currentPos);
+        checkInsideMaze(currentPos, directions);
         checkIfFinished(currentPos, finishPos);
         break;
     }
-    // console.log("you are at:", currentPos);
+    console.log("you are at:", currentPos);
   });
-  if (!safePath) return "Dead";
-  if (finished) return "Finish";
+  if (!safePath || outsideMaze) return "Dead";
+  else if (finished) return "Finish";
   return "Lost";
 }
 
 function checkIsSafe(currentPos) {
   const currentRow = maze[currentPos.y];
-  if (!currentRow) {
-    safePath = !safePath;
+  if (!currentRow || maze[currentPos.y][currentPos.x] === key.wall) {
+    safePath = false;
     return safePath;
   }
-  if (maze[currentPos.y][currentPos.x] === key.wall) {
-    safePath = !safePath;
-    return safePath;
+}
+
+function checkInsideMaze(currentPos, directions) {
+  if (currentPos.x < 0 || currentPos.y < 0) {
+    console.log("outside of maze 1:", currentPos);
+    outsideMaze = true;
+    return outsideMaze;
   }
-  if (
-    currentPos.x < 0 ||
-    currentPos.y < 0 ||
-    currentPos.x > maze.length ||
-    currentPos.y > maze.length
-  ) {
-    safePath = !safePath;
-    return safePath;
+  if (currentPos.x >= maze.length || currentPos.y >= maze.length) {
+    console.log("outside of maze 2:", currentPos);
+    console.log("directions", directions);
+    console.log("maze:", maze);
+    console.log("maze size:", maze.length);
+    outsideMaze = true;
+    return outsideMaze;
   }
 }
 
 function checkIfFinished(currentPos, finishPos) {
   if (currentPos.x === finishPos.x && currentPos.y === finishPos.y) {
-    finished = !finished;
+    finished = true;
     return finished;
   }
 }
 
-console.log("0", mazeRunner(maze, []), "Expected Lost");
+// console.log("0", mazeRunner(maze, []), "Expected Lost");
+
+// console.log(
+//   "1",
+//   mazeRunner(maze, ["N", "N", "N", "N", "N", "E", "E", "E", "E", "E"]),
+//   "Expected Finish"
+// );
+// console.log(
+//   "2",
+//   mazeRunner(maze, [
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "E",
+//     "E",
+//     "S",
+//     "S",
+//     "E",
+//     "E",
+//     "N",
+//     "N",
+//     "E",
+//   ]),
+//   "Expected Finish"
+// );
+// console.log(
+//   "3",
+//   mazeRunner(maze, [
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "E",
+//     "E",
+//     "E",
+//     "E",
+//     "E",
+//     "W",
+//     "W",
+//   ]),
+//   "Expected Finish"
+// );
+
+// console.log("4", mazeRunner(maze, ["N", "N", "N", "W", "W"]), "Expected Dead");
+// console.log(
+//   "5",
+//   mazeRunner(maze, [
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "N",
+//     "E",
+//     "E",
+//     "S",
+//     "S",
+//     "S",
+//     "S",
+//     "S",
+//     "S",
+//   ]),
+//   "Expected Dead"
+// );
+
+// console.log("6", mazeRunner(maze, ["N", "E", "E", "E", "E"]), "Expected Lost");
 
 console.log(
-  "1",
-  mazeRunner(maze, ["N", "N", "N", "N", "N", "E", "E", "E", "E", "E"]),
-  "Expected Finish"
+  "7",
+  mazeRunner(
+    [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 3],
+      [1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1],
+      [1, 2, 1, 0, 1, 0, 1],
+    ],
+    [
+      "N",
+      "N",
+      "N",
+      "W",
+      "W",
+      "W",
+      "N",
+      "N",
+      "W",
+      "W",
+      "S",
+      "S",
+      "S",
+      "S",
+      "W",
+      "W",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+    ]
+  )
 );
-console.log(
-  "2",
-  mazeRunner(maze, [
-    "N",
-    "N",
-    "N",
-    "N",
-    "N",
-    "E",
-    "E",
-    "S",
-    "S",
-    "E",
-    "E",
-    "N",
-    "N",
-    "E",
-  ]),
-  "Expected Finish"
-);
-console.log(
-  "3",
-  mazeRunner(maze, [
-    "N",
-    "N",
-    "N",
-    "N",
-    "N",
-    "E",
-    "E",
-    "E",
-    "E",
-    "E",
-    "W",
-    "W",
-  ]),
-  "Expected Finish"
-);
-
-console.log("4", mazeRunner(maze, ["N", "N", "N", "W", "W"]), "Expected Dead");
-console.log(
-  "5",
-  mazeRunner(maze, [
-    "N",
-    "N",
-    "N",
-    "N",
-    "N",
-    "E",
-    "E",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-  ]),
-  "Expected Dead"
-);
-
-console.log("6", mazeRunner(maze, ["N", "E", "E", "E", "E"]), "Expected Lost");
