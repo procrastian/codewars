2365f140f111937732916c91688538f203d0f375
Your Name <your_email@address>
2023-09-08 10:21:11 +0100

    simple solution for first non repeating character

--- incomplete/first_non-repeating_character/index.js
+++ incomplete/first_non-repeating_character/index.js
@@ -1,3 +1,14 @@
 function firstNonRepeatingLetter(s) {
-  
+  const letters = s.split("");
+  let letterPosition = 0;
+  let fnrLetter = letters[0];
+  for (let i = 1; i < letters.length; i++) {
+    if (letters[i] === fnrLetter) {
+      letterPosition++
+      fnrLetter = letters[letterPosition]
+    }
+  }
+  return fnrLetter;
 }
+
+console.log(firstNonRepeatingLetter("moonmen"));
